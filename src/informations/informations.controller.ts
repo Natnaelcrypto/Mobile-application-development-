cdf5f48a6cf65d18216f2e9651758c08822cf365
Firaol Bogale <firaolbogala98@gmail.com>
2023-05-06 22:12:55 +0300

    protected the informations file routes and updated the packeckg.json

--- src/informations/informations.controller.ts
+++ src/informations/informations.controller.ts
@@ -7,34 +7,46 @@ import {
   Patch,
   Post,
   Request,
+  UseGuards,
+  
 } from "@nestjs/common";
 import { InformationsService } from "./informations.service";
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller("api/v1/informations")
+@Controller('api/v1/informations')
 export class InformationsController {
   constructor(private readonly informationService: InformationsService) {}
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   find_infos(@Request() req: any) {
     const find_user = req.user;
     return this.informationService.find_infos();
   }
+
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   find_info(@Body() data: any, @Request() req: any) {
     const find_user = req.user;
     return this.informationService.find_info(data);
   }
+
+  @UseGuards(AuthGuard('jwt'))
   @Post()
-  create_info(@Body() data:any, @Request() req: any) {
+  create_info(@Body() data: any, @Request() req: any) {
     const find_user = req.user;
     return this.informationService.create(data);
   }
-  @Patch(":id")
-  update(@Param("id") id: any, @Body() data:any, @Request() req: any) {
+
+  @UseGuards(AuthGuard('jwt'))
+  @Patch(':id')
+  update(@Param('id') id: any, @Body() data: any, @Request() req: any) {
     const find_user = req.user;
     return this.informationService.Update_info(id, data);
   }
-  @Delete("id")
-  delate(@Param("id") id: any, @Request() req: any) {
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete('id')
+  delate(@Param('id') id: any, @Request() req: any) {
     const find_user = req.user;
     return this.informationService.delate(id);
   }
