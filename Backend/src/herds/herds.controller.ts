422c099f6011da6e7cb60e3f31941dd349f29b46
Basliel Amsalu <119809588+Basliel-Amsalu@users.noreply.github.com>
2023-06-02 19:28:53 +0300

    Update herds.controller.ts

--- Backend/src/herds/herds.controller.ts
+++ Backend/src/herds/herds.controller.ts
@@ -9,43 +9,34 @@ import {
   Patch,
   Post,
   Request,
-  UseGuards,
 } from '@nestjs/common';
 
 @Controller('api/v1/herds')
 export class HerdsController {
   constructor(private readonly herdService: HerdsService) {}
 
-  @UseGuards(AuthGuard('jwt'))
-  @Get(':id')
+  @Get('single/:id')
   find_herd(@Param('id') id: any, @Request() req: any) {
-    const find_user = req.user;
     return this.herdService.find_herd(id);
   }
 
-  @UseGuards(AuthGuard('jwt'))
-  @Get()
-  find_herds(@Body() date: any, @Request() req: any) {
-    const find_user = req.user;
-    return this.herdService.find_herds(date);
+  @Get(':farmname')
+  find_herds(@Param('farmname') farmname: any) {
+    return this.herdService.find_herds(farmname);
   }
-  @UseGuards(AuthGuard('jwt'))
+
   @Post()
   create_herd(@Body() date: any, @Request() req: any) {
-    const find_user = req.user;
     return this.herdService.create_herd(date);
   }
-  @UseGuards(AuthGuard('jwt'))
   @Patch(':id')
-  update_herd(@Body() data: any, @Param() id: any, @Request() req: any) {
-    const find_user = req.user;
+  update_herd(@Body() data: any, @Param() id: any) {
     return this.herdService.Update_herd(id, data);
   }
-  
-  @UseGuards(AuthGuard('jwt'))
+
+
   @Delete(':id')
   delate_herd(@Param('id') herdId, @Request() req: any) {
-    const find_user = req.user;
     return this.herdService.delate(herdId);
   }
 }
