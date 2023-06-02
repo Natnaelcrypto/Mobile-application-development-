30a7a0af66e78278154512fd7d788dd85a4e43e2
Dawit M <108027360+DawitMinale@users.noreply.github.com>
2023-06-02 20:06:07 +0300

    Update farms.service.ts

--- Backend/src/farms/farms.service.ts
+++ Backend/src/farms/farms.service.ts
@@ -13,55 +13,68 @@ export class FarmsService {
       return farm;
     }
     throw new BadRequestException('farm dose not exist');
-    
   }
 
-  async find_farmes() {
-    const farms = await this.farmModel.find();
+  async find_farmes(farmname: any) {
+    const farms = await this.farmModel.find({ farmName: farmname });
     if (farms) {
       return farms;
     }
     throw new BadRequestException('There is no farm');
-    
   }
 
   async create_farm(data: any) {
+    const new_farm = new this.farmModel({
+      farmName: data.farmName,
+      userID: data.userID,
+      expirationDate: data.expirationDate,
+      itemName: data.itemName,
+      dosage: data.dosage,
+      instructions: data.instructions,
+      brand: data.brand,
+      type: data.type,
+      quantity: data.quantity,
+      isfeed: data.isfeed,
+      ismedication: data.ismedication,
+    });
 
-    const new_farm= new this.farmModel({
-        farmName:data.farmName,
-        userID:data.userID,
-        inventory:data.inventory
-    })
-
-    const result = await new_farm.save()
-    return result
+    const result = await new_farm.save();
+    return result;
   }
 
-  async update(data:any,id:any){
-    const farm= await this.farmModel.findById(id);
-    if (!farm){
-        throw new BadRequestException('farm does not exist');
-        
+  async update(data: any, id: any) {
+    const farm = await this.farmModel.findById(id);
+    if (!farm) {
+      throw new BadRequestException('farm does not exist');
     }
-    if (data.inventory){
-        farm.inventory=data.inventory
+    if (data.brand) {
+      farm.brand = data.brand;
+    }
+    if (data.type) {
+      farm.type = data.type;
+    }
+    if (data.expirationDate) {
+      farm.expirationDate = data.expirationDate;
+    }
+    if (data.dosage) {
+      farm.dosage = data.dosage;
     }
     if (data.farmName) {
       farm.farmName = data.farmName;
     }
-    if (data.userID) {
-      farm.userID = data.userID;
+    if (data.itemName) {
+      farm.itemName = data.itemName;
     }
-    const result = await farm.save()
-    return result
+
+    const result = await farm.save();
+    return result;
   }
 
-  async delate_farm(farm_Id:any){
+  async delate_farm(farm_Id: any) {
     const farm = await this.farmModel.findById(farm_Id);
-    if (farm){
-        return "farm delated"
+    if (farm) {
+      return 'farm delated';
     }
-    throw new BadRequestException("Error occured");
-   
+    throw new BadRequestException('Error occured');
   }
 }
