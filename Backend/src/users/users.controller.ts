697628d19752bd2ee53d697b093e5fc287044b4d
YaredBT <110673168+YaredBT@users.noreply.github.com>
2023-06-02 21:46:07 +0300

    Update users.controller.ts

--- Backend/src/users/users.controller.ts
+++ Backend/src/users/users.controller.ts
@@ -1,26 +1,36 @@
-import { Body, Controller, Get, Param, Patch, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('api/v1/users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
-  @Get()
-  find_users() {
-    return this.userService.find_users();
+  @Get(':farmname')
+  find_users(@Param('farmname') farmname: any) {
+    console.log(farmname);
+    return this.userService.find_users(farmname);
   }
 
-  @Get('user')
-  find_user(@Body() user: any) {
-    return this.userService.find_user(user);
+  @Patch(':id')
+  update_user(@Body() user: any, @Param('id') id: any) {
+    return this.userService.update_user(id,user);
   }
 
-  @Patch()
-  update_user(@Body() user: any) {
-    return this.userService.update_user(user);
+  @Post()
+  create_user(@Body() user: any) {
+    return this.userService.create_User(user);
   }
 
   @Delete(':id')
   delete_users(@Param('id') id: any) {
+    console.log(id);
     return this.userService.delate_user(id);
   }
 }
